bd5ebf241bbf0c5102134a7b2345f9a936586379
Jan Stanstrup <stanstrup@gmail.com>
2017-11-08 10:31:45 +0100

    CompDb
    Replaced `CompoundDb` with `CompDb`. For #20

--- inst/pkg-template/R/zzz.R
+++ inst/pkg-template/R/zzz.R
@@ -8,7 +8,7 @@
   path <- system.file("extdata", package = pkgname, lib.loc = libname)
   files <- dir(path)
   for(i in seq_len(length(files))){
-      db <- CompoundDb(system.file("extdata", files[[i]], package = pkgname,
+      db <- CompDb(system.file("extdata", files[[i]], package = pkgname,
                                    lib.loc = libname))
       objname <- sub(".sqlite$","",files[[i]])
       assign(objname, db, envir = ns)
