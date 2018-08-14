fe0448fdcebb3cef97e707c05a2d685015cbd02d
jotsetung <johannes.rainer@gmail.com>
2018-08-14 06:48:16 +0200

    Add accessor methods for Spectrum2List

--- R/Spectrum2List-methods.R
+++ R/Spectrum2List-methods.R
@@ -60,7 +60,7 @@ setReplaceMethod("$", "Spectrum2List", function(x, name, value) {
 #' @section Accessing spectrum attributes:
 #'
 #' These methods allow to access the attributes and values of the individual
-#' `Spectrum2` objects. For more details please refer to the corresoonding
+#' `Spectrum2` objects. For more details please refer to the corresponding
 #' methods in the `MSnbase` package
 #'
 #' - `mz` return the m/z values of each spectrum as a `list` of `numeric`
@@ -72,6 +72,42 @@ setReplaceMethod("$", "Spectrum2List", function(x, name, value) {
 #' - `rtime` return the retention time of each spectrum as a `numeric` vector
 #'   with length equal to the length of `object`.
 #'
+#' - `precursorMz`, `precursorCharge`, `precursorIntensity`, `precScanNum`
+#'   return precursor m/z values, charge, intensity and scan number for each
+#'   spectrum as a `numeric` (or `integer`) vector with length equal to the
+#'   length of `object`.
+#'
+#' - `acquisitionNum` and `scanIndex` return the acquisition number of each
+#'   spectrum and its scan index as an `integer` vector with the same length
+#'   than `object`.
+#'
+#' - `ionCount` and `tic` return the ion count and total ion current of each
+#'   spectrum.
+#'
+#' - `peaksCount` returns the number of peaks for each spectrum as a `integer`
+#'   vector.
+#'
+#' - `msLevel` returns the MS level of each spectrum.
+#'
+#' - `collisionEnergy` returns the collision energy for each spectrum.
+#'
+#' - `polarity` returns the spectra's polarity.
+#'
+#' - `fromFile` returns the index from the (e.g. mzML) file the spectra where
+#'   from. This applies only for spectra read using e.g. `MSnbase`'s
+#'   `readMSData` function.
+#'
+#' - `smoothed` whether spectra have been smoothed (i.e. processed with the
+#'   [MSnbase::smooth] method. Returns a `logical` of length equal to the
+#'   number of spectra.
+#'
+#' - `isEmpty` returns `TRUE` for spectra without peak data.
+#'
+#' - `centroided`, `isCentroided` returns for each spectrum whether it contains
+#'   *centroided* data. While `centroided` returns the internal attribute of
+#'   each spectrum, `isCentroided` tries to guess whether spectra are
+#'   centroided from the actual peak data.
+#' 
 #' @importMethodsFrom MSnbase mz
 #' 
 #' @export
@@ -112,23 +148,260 @@ setMethod("rtime", "Spectrum2List", function(object) {
     vapply(object, function(z) if(length(z@rt)) z@rt else NA_real_, numeric(1))
 })
 
-## precursorMz
-## precursorCharge
-## precursorIntensity
-## acquisitionNum
-## scanIndex
-## precScanNum
-## peaksCount
-## mzLevel
-## collisionEnergy
-## tic
-## ionCount
-## fromFile
-## polarity
-## centroided
-## smoothed
-## isEmpty
-## isCentroided
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase precursorMz
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the precursor m/z of each spectrum.
+#' precursorMz(spl)
+setMethod("precursorMz", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@precursorMz)) z@precursorMz else NA_real_, numeric(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase precursorCharge
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the precursor charge of each spectrum.
+#' precursorCharge(spl)
+setMethod("precursorCharge", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@precursorCharge)) z@precursorCharge else NA_integer_,
+        integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase precScanNum
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the precursor scan number for each spectrum.
+#' precScanNum(spl)
+setMethod("precScanNum", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@precScanNum)) z@precScanNum else NA_integer_,
+        integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase precursorIntensity
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the precursor intensity of each spectrum.
+#' precursorIntensity(spl)
+setMethod("precursorIntensity", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@precursorIntensity)) z@precursorIntensity else NA_real_,
+        numeric(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase acquisitionNum
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the acquisition number of each spectrum.
+#' acquisitionNum(spl)
+setMethod("acquisitionNum", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@acquisitionNum)) z@acquisitionNum else NA_integer_,
+        integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase scanIndex
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the scan index of each spectrum.
+#' scanIndex(spl)
+setMethod("scanIndex", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@scanIndex)) z@scanIndex else NA_integer_,
+        integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase peaksCount
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Get the number of peaks per spectrum.
+#' peaksCount(spl)
+setMethod("peaksCount", "Spectrum2List", function(object) {
+    vapply(object, peaksCount, integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase msLevel
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Get the MS level of each spectrum.
+#' msLevel(spl)
+setMethod("msLevel", "Spectrum2List", function(object) {
+    vapply(object, msLevel, integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase tic
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Get the total ion current for each spectrum.
+#' tic(spl)
+setMethod("tic", "Spectrum2List", function(object) {
+    vapply(object, tic, numeric(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase ionCount
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Get the total ion current for each spectrum.
+#' ionCount(spl)
+setMethod("ionCount", "Spectrum2List", function(object) {
+    vapply(object, ionCount, numeric(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase collisionEnergy
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the collision energy for spectrum.
+#' collisionEnergy(spl)
+setMethod("collisionEnergy", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@collisionEnergy)) z@collisionEnergy else NA_real_,
+        numeric(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase fromFile
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Extract the file index for the spectrum.
+#' fromFile(spl)
+setMethod("fromFile", "Spectrum2List", function(object) {
+    vapply(object, function(z)
+        if(length(z@fromFile)) z@fromFile else NA_integer_,
+        integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase polarity
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Get the polarity for each spectrum.
+#' polarity(spl)
+setMethod("polarity", "Spectrum2List", function(object) {
+    vapply(object, polarity, integer(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase smoothed
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Whether spectra are smoothed (i.e. processed with the `MSnbase::smooth`
+#' ## function.
+#' smoothed(spl)
+setMethod("smoothed", "Spectrum2List", function(object) {
+    vapply(object, smoothed, logical(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase isEmpty
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Are spectra empty (i.e. contain no peak data)?
+#' isEmpty(spl)
+setMethod("isEmpty", "Spectrum2List", function(x) {
+    vapply(x, isEmpty, logical(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase centroided
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Do the spectra contain centroided data?
+#' centroided(spl)
+setMethod("centroided", "Spectrum2List", function(object) {
+    vapply(object, centroided, logical(1))
+})
+
+#' @rdname Spectrum2List
+#' 
+#' @importMethodsFrom MSnbase isCentroided
+#' 
+#' @export
+#'
+#' @examples
+#'
+#' ## Do the spectra contain centroided data? Whether spectra are centroided
+#' ## is estimated from the peak data.
+#' isCentroided(spl)
+setMethod("isCentroided", "Spectrum2List", function(object) {
+    vapply(object, isCentroided, logical(1))
+})
 
 ## clean, all = FALSE
 ## removePeaks, t
--- tests/testthat/test_Spectrum2List-methods.R
+++ tests/testthat/test_Spectrum2List-methods.R
@@ -30,3 +30,106 @@ test_that("mz, intensity, rtime work", {
     expect_equal(names(mz(spl)), c("a", "b", "c", "d", "e"))
     expect_equal(names(intensity(spl)), c("a", "b", "c", "d", "e"))
 })
+
+test_that("precursor* work", {
+    sp1 <- new("Spectrum2", precursorMz = 123.3, precursorCharge = 1L,
+               precursorIntensity = 1234.4)
+    sp2 <- new("Spectrum2", precursorMz = NA_real_, precursorCharge = integer(),
+               precursorIntensity = NA_real_, precScanNum = 34L)
+    spl <- Spectrum2List(sp1, sp2)
+    expect_equal(precursorMz(spl), c(123.3, NA))
+    expect_equal(precursorCharge(spl), c(1L, NA))
+    expect_true(is.integer(precursorCharge(spl)))
+    expect_equal(precursorIntensity(spl), c(1234.4, NA))
+    expect_equal(precScanNum(spl), c(NA_integer_, 34L))
+    
+    expect_equal(precursorMz(spl_), rep(NA_real_, length(spl_)))
+    expect_equal(precursorCharge(spl_), rep(NA_integer_, length(spl_)))
+    expect_equal(precursorIntensity(spl_), rep(NA_real_, length(spl_)))
+    expect_true(is.integer(precScanNum(spl_)))
+})
+
+test_that("acquisitionNum and scanIndex work", {
+    sp1 <- new("Spectrum2", acquisitionNum = 2L, scanIndex = 1L)
+    sp2 <- new("Spectrum2", acquisitionNum = 4L)
+    spl <- Spectrum2List(sp1, sp2)
+    expect_identical(acquisitionNum(spl), c(2L, 4L))
+    expect_identical(scanIndex(spl), c(1L, NA_integer_))
+        
+    expect_equal(acquisitionNum(spl_), rep(NA_integer_, length(spl_)))
+    expect_equal(scanIndex(spl_), rep(NA_integer_, length(spl_)))
+    expect_true(is.integer(acquisitionNum(spl_)))
+    expect_true(is.integer(scanIndex(spl_)))
+})
+
+test_that("peaksCount, msLevel, tic and ionCount work", {
+    sp1 <- new("Spectrum2", msLevel = 3L, tic = 5)
+    sp2 <- new("Spectrum2")
+    spl <- Spectrum2List(sp1, sp2)
+
+    expect_true(is.integer(peaksCount(spl)))
+    expect_equal(peaksCount(spl), c(0, 0))
+    expect_true(is.integer(msLevel(spl)))
+    expect_equal(msLevel(spl), c(3, 2))
+    expect_true(is.numeric(tic(spl)))
+    expect_equal(tic(spl), c(5, 0))
+    expect_true(is.numeric(ionCount(spl)))
+    expect_equal(ionCount(spl), c(0, 0))
+
+    expect_equal(peaksCount(spl_), c(5, 7, 33, 10))
+    expect_true(all(msLevel(spl_) == 2))
+    expect_true(all(is.na(tic(spl_))))
+    expect_equal(ionCount(spl_), unlist(lapply(intensity(spl_), sum)))
+})
+
+test_that("collisionEnergy works", {
+    sp1 <- new("Spectrum2")
+    sp2 <- new("Spectrum2", collisionEnergy = 23.3)
+    spl <- Spectrum2List(sp1, sp2)
+
+    expect_true(is.numeric(collisionEnergy(spl)))
+    expect_equal(collisionEnergy(spl), c(NA, 23.3))
+
+    expect_true(is.numeric(collisionEnergy(spl_)))
+    expect_equal(collisionEnergy(spl_), c(10, 25, NA, 20))
+})
+
+test_that("fromFile and polarity work", {
+    sp1 <- new("Spectrum2", polarity = 1L, fromFile = 5L)
+    sp2 <- new("Spectrum2", fromFile = 3L)
+    spl <- Spectrum2List(sp1, sp2)
+
+    expect_true(is.integer(fromFile(spl)))
+    expect_equal(fromFile(spl), c(5, 3))
+    expect_true(is.integer(polarity(spl)))
+    expect_equal(polarity(spl), c(1, NA))
+
+    expect_true(is.integer(fromFile(spl_)))
+    expect_true(all(is.na(fromFile(spl_))))
+    expect_true(is.integer(polarity(spl_)))
+    expect_equal(polarity(spl_), c(1, 1, 1, 0))
+})
+
+test_that("smoothed, isEmpty, centroided and isCentroided work", {
+    sp1 <- new("Spectrum2", mz = c(1, 2, 3, 4), intensity = c(4, 2, 4, 5))
+    sp2 <- new("Spectrum2", centroided = TRUE, smoothed = TRUE)
+    spl <- Spectrum2List(sp1, sp2)
+
+    expect_true(is.logical(smoothed(spl)))
+    expect_equal(smoothed(spl), c(NA, TRUE))
+    expect_true(is.logical(isEmpty(spl)))
+    expect_equal(isEmpty(spl), c(FALSE, TRUE))
+    expect_true(is.logical(centroided(spl)))
+    expect_equal(centroided(spl), c(NA, TRUE))
+    expect_true(is.logical(isCentroided(spl)))
+    expect_equal(isCentroided(spl), c(NA, NA))
+
+    expect_true(is.logical(smoothed(spl_)))
+    expect_equal(smoothed(spl_), rep(NA, length(spl_)))
+    expect_true(is.logical(isEmpty(spl_)))
+    expect_true(all(!isEmpty(spl_)))
+    expect_true(is.logical(centroided(spl_)))
+    expect_true(all(is.na(centroided(spl_))))
+    expect_true(is.logical(isCentroided(spl_)))
+    expect_equal(isCentroided(spl_), c(NA, NA, TRUE, NA))
+})
