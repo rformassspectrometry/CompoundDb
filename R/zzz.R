6216ac6904d22d6a3aed5766ffaaa4ca158322a9
jotsetung <johannes.rainer@gmail.com>
2019-03-15 06:43:05 +0100

    Store the adduct definition as data.frame

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,6 @@
 .onLoad <- function(libname, pkgname) {
     dta <- as.data.frame(rbind(commonMZ::MZ_CAMERA("pos", warn_clash = FALSE),
                                commonMZ::MZ_CAMERA("neg", warn_clash = FALSE)))
-    add_env <- list2env(split(dta, dta$name))
-    assign("ADDUCTS", add_env, envir = asNamespace(pkgname))
+    rownames(dta) <- dta$name
+    assign("ADDUCTS", dta, envir = asNamespace(pkgname))
 }
