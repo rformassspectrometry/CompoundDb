33ed44c22857a42380a2d7b706437c2e5ae2d938
jotsetung <johannes.rainer@gmail.com>
2019-03-19 13:01:11 +0100

    Add annotateMz function (issue #39)
    - Add annotateMz to annotate provided m/z values.
    - Add related documentation and unit tests.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,1 +1,12 @@
 setGeneric("hasMz", function(object, mz, ...) standardGeneric("hasMz"))
+
+setGeneric("annotateMz", function(object, compounds, ...)
+    standardGeneric("annotateMz"))
+
+#' @importClassesFrom tibble tbl_df
+#'
+#' @noRd
+setClassUnion("DataFrameOrEquivalent", c("data.frame", "DataFrame", "tbl_df"))
+
+setClassUnion("numericOrDataFrameOrEquivalent",
+              c("numeric", "DataFrameOrEquivalent"))
--- R/mass-utility-functions.R
+++ R/mass-utility-functions.R
@@ -4,8 +4,6 @@
     ppm * x / 1e6
 }
 
-## function with mass as input and ppm to search for compound in mass.
-
 #' @title Match values allowing a certain difference in ppm
 #'
 #' @description
@@ -148,3 +146,192 @@ adducts <- function(pattern, polarity, name, set, ...) {
         adds <- adds[match(name, adds$name), ]
     adds
 }
+
+#' @description
+#'
+#' Given m/z values and names of potential adducts they represent, get all
+#' rows of the `cmps` data frame with a matching mass (allowing `ppm`
+#' difference).
+#'
+#' @param x `numeric` with m/z values.
+#'
+#' @param cmps `data.frame` with a column containing monoisotopic masses in a
+#'     column named `"mass"`.
+#'
+#' @param adduct Adduct definition. Either a `data.frame` as returned by
+#'     [adducts()] or a `character` with the names of the adducts in that
+#'     `data.frame`.
+#'
+#' @param ppm `numeric(1)` defining the allowed mass difference in ppm.
+#'
+#' @return a `list` of `data.frame`. Each element containing the results for
+#'     each input m/z. Column `"ppm"` in the result `data.frame`s contains the
+#'     mass difference in ppm. If nothing is found a `data.frame` with `NA`
+#'     values is returned.
+#'
+#' @noRd
+#'
+#' @examples
+#'
+#' x <- c(76.039304409, 127.036558, 197.078426936, 160.0756)
+#' names(x) <- c("Glycine", "3-Hydroxybutyric Acid", "Suberic Acid",
+#'     "Serotonin")
+#' ## The reported masses are for adducts:
+#' ## Glycine: [M+H]+
+#' ## 3-Hydroxybutyric Acid: [M+Na]+
+#' ## Suberic Acid: [M+Na]+
+#' ## Serotonin: [M-NH3+H]+
+#'
+#' cmps <- compounds(cdb)
+#' res <- CompoundDb:::.annotate_adduct_mz(x, cmps, ppm = 5, adduct = c("[M+H]+", "[M+Na]+"))
+#'
+#' res <- CompoundDb:::.annotate_adduct_mz(x, cmps, ppm = 5, adduct = c("[M+H]+", "[M-NH3+H]+"))
+#'
+#' ## Negative
+#' x <- c(266.089709, 219.0433)
+#' names(x) <- c("Adenosine", "Phosphorylcholine")
+#'
+#' ## Adenosine: [M-H]-
+#' ## Phosphorylcholine: [M+Cl]-
+#' res <- CompoundDb:::.annotate_adduct_mz(x, cmps, ppm = 5, adduct = c("[M-H]-", "[M+Cl]-"))
+.annotate_adduct_mz <- function(x, cmps, adduct = adducts(), ppm = 10) {
+    if (is.character(adduct))
+        adduct <- adducts(name = adduct)
+    adduct$charge <- abs(adduct$charge)
+    not_found <- cmps[1, , drop = FALSE]
+    rownames(not_found) <- NULL
+    not_found[1, ] <- lapply(sapply(not_found, class), as, object = NA)
+    not_found$adduct <- NA_character_
+    not_found$ppm <- NA_real_
+    lapply(x, function(z) {
+        mss <- (adduct$charge * z - adduct$massdiff) / adduct$nmol
+        idx <- matchWithPpm(mss, cmps$mass, ppm = ppm)
+        hits <- lengths(idx)
+        idx <- unlist(idx, use.names = FALSE)
+        if (length(idx)) {
+            res <- cmps[idx, , drop = FALSE]
+            rownames(res) <- NULL
+            res$adduct <- rep(adduct$name, hits)
+            rep_mss <- rep(mss, hits)
+            res$ppm <- abs(res$mass - rep_mss) * 1e6 / res$mass
+            res[order(res$ppm), ]
+        } else not_found
+    })
+}
+
+#' @title Annotate m/z values for ion adducts
+#'
+#' @aliases annotateMz
+#'
+#' @rdname annotateMz
+#'
+#' @description
+#'
+#' `annotateMz` annotates m/z values using either a `data.frame` containing
+#' compound definitions or a [CompDb()] and a user provided list of potential
+#' (expected) adducts. In detail, the function calculates for each input m/z
+#' value the masses of the potential ion adducts and compares these with
+#' (monoisotopic) masses provided in the reference annotation. A hit is returned
+#' if the mass difference is smaller than `ppm`.
+#'
+#' For `object` being a `numeric` and `compounds` a `data.frame` or equivalend
+#' class, the function returns a `list` of subsets of `compounds` with rows
+#' matching the adducts' mass. If no match is found for the mass of any
+#' adducts, a row of `compounds` with all values set to `NA` is returned. Note
+#' that if `compounds` is ` [CompDb()], a `list` of `data.frame`s is returned.s
+#'
+#' @param object either a `numeric` of m/z values or a `data.frame` (or
+#'     equivalent) with a column containing the m/z values.
+#'
+#' @param compounds either a `data.frame` (or equivalent) or a [CompDb()] with
+#'     the reference annotations. A column named `"mass"` is mandatory if
+#'     `compounds` is a `data.frame`.
+#'
+#' @param adduct adduct definition. Either a `data.frame` as returned by
+#'     [adducts()] or a `character` with the names of the adduct(s) in that
+#'     `data.frame`.
+#'
+#' @param ppm `numeric(1)` defining the allowed mass difference in ppm.
+#'
+#' @param mzcol for `object` being a `data.frame`: `character(1)` defining the
+#'     column name in `object` containing the m/z values. Defaults to `"mz"`.
+#'
+#' @param ... additional parameters for the `compounds` call if `compounds` is
+#'     a `CompDb`.
+#'
+#' @return See description above for details.
+#'
+#' @author Johannes Rainer
+#'
+#' @export
+#'
+#' @md
+#'
+#' @examples
+#'
+#' ## Read compound annotations from a package-internal SDF file containing
+#' ## a small subset of HMDB annotations
+#' fl <- system.file("sdf/HMDB_sub.sdf.gz", package = "CompoundDb")
+#' cmps <- compound_tbl_sdf(fl)
+#' cmps
+#'
+#' ## Annotate provided m/z values assuming they represent mass-to-charge
+#' ## values for [M+H]+ adducts.
+#' mzs <- c(105.0546, 75.09168, 127.0365, 113.04756, 113.1210)
+#'
+#' res <- annotateMz(mzs, cmps, adduct = "[M+H]+")
+#'
+#' ## res is a `list` with the matching hits for each m/z and the specified
+#' ## adduct. If no match was found the result contains only `NA` values.
+#' res
+#'
+#' ## Annotating using all adducts defined by `adducts()` and accepting a
+#' ## 20ppm difference between the masses
+#' annotateMz(mzs, cmps, ppm = 20)
+#'
+#' ## It is also possible to annotate an input data.frame, in which case each
+#' ## input will be duplicated depending on the number of hits for each m/z
+#' mzs_df <- data.frame(id = letters[1:5], mzmed = mzs)
+#'
+#' res <- annotateMz(mzs_df, cmps, mzcol = "mzmed", adduct = "[M+H]+")
+#' res
+#'
+#' ## Note that `compounds` could also be `CompDb` database, in which a search
+#' ## against the annotations provided in that database would be performed.
+setMethod(
+    "annotateMz", signature(object = "numeric",
+                            compounds = "DataFrameOrEquivalent"),
+    function(object, compounds, adduct = adducts(), ppm = 10, ...) {
+        if (!any(colnames(compounds) == "mass"))
+            stop("Required column \"mass\" not found in 'compounds'",
+                 call. = FALSE)
+        .annotate_adduct_mz(object, compounds, adduct = adduct, ppm = ppm)
+    })
+
+#' @rdname annotateMz
+setMethod(
+    "annotateMz", signature(object = "DataFrameOrEquivalent",
+                            compounds = "DataFrameOrEquivalent"),
+    function(object, compounds, adduct = adducts(), ppm = 10,
+             mzcol = "mz", ...) {
+        if (!any(colnames(object) == mzcol))
+            stop("Column \"", mzcol, "\" with input m/z values not found ",
+                 "in 'object'", call. = FALSE)
+        res <- annotateMz(object[, mzcol], compounds = compounds,
+                          adduct = adduct, ppm = ppm)
+        nrows <- vapply(res, nrow, integer(1))
+        object <- object[rep(1:length(res), nrows), , drop = FALSE]
+        rownames(object) <- NULL
+        cbind(object, do.call(rbind, res))
+    }
+)
+
+#' @rdname annotateMz
+setMethod(
+    "annotateMz", signature(object = c("numericOrDataFrameOrEquivalent"),
+                            compounds = "CompDb"),
+    function(object, compounds, adduct = adducts(), ppm = 10, ...) {
+        annotateMz(object, compounds(x = compounds, ...), adduct = adduct,
+                   ppm = ppm, ...)
+    }
+)
